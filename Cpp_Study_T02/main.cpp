155184d161f0d43b4defbf7f50aabdf7ebca5c74
ssas <ssas@ssasui-MacBook-Pro.local>
2015-05-03 23:01:45 +0900

    Initial Commit

---
+++ Cpp_Study_T02/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Cpp_Study_T02
+//
+//  Created by ssas on 2015. 5. 3..
+//  Copyright (c) 2015년 ShockRabbit. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
